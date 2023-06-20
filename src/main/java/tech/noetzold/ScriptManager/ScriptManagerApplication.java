c131d0bfa12837da5e91a8bec448afe9d3736acc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-20 18:51:11 -0300

    Initial commit

---
+++ src/main/java/tech/noetzold/ScriptManager/ScriptManagerApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.ScriptManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptManagerApplication.class, args);
+	}
+
+}
