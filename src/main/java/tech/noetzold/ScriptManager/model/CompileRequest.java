b77116db4858311f2b532cfa8ffcfd3a90737947
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-22 11:32:23 -0300

    Update CompileRequest.java

--- src/main/java/tech/noetzold/ScriptManager/model/CompileRequest.java
+++ src/main/java/tech/noetzold/ScriptManager/model/CompileRequest.java
@@ -1,6 +1,9 @@
 package tech.noetzold.ScriptManager.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 public class CompileRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String source;
     private String lang;
