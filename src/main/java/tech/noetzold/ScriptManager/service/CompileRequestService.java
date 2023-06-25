9da823de8139ccfd1e089b3d575b19110b7054bb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-25 07:08:35 -0300

    Update CompileRequestService.java

--- src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
+++ src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
@@ -1,8 +1,6 @@
 package tech.noetzold.ScriptManager.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tech.noetzold.ScriptManager.client.HackerEarthClient;
 
 @Service
 public class CompileRequestService {
