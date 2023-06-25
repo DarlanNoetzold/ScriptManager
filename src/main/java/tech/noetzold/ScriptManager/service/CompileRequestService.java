cce9cd2b886b83fb9882f7c0c071403958a9ec66
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-25 06:49:28 -0300

    Create CompileRequestService.java

---
+++ src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
@@ -0,0 +1,10 @@
+package tech.noetzold.ScriptManager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.ScriptManager.client.HackerEarthClient;
+
+@Service
+public class CompileRequestService {
+
+}
