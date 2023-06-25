35e76dc20507a1546b5aaff07a52b7b25915c9b6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-25 06:49:31 -0300

    Create CodeCompilationService.java

---
+++ src/main/java/tech/noetzold/ScriptManager/service/CodeCompilationService.java
@@ -0,0 +1,24 @@
+package tech.noetzold.ScriptManager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.ScriptManager.client.HackerEarthClient;
+import tech.noetzold.ScriptManager.model.CompileRequest;
+
+public class CodeCompilationService {
+    private final HackerEarthClient hackerEarthClient;
+
+    @Autowired
+    public CodeCompilationService(HackerEarthClient hackerEarthClient) {
+        this.hackerEarthClient = hackerEarthClient;
+    }
+
+    public String compileCode(String code) {
+        CompileRequest compileRequest = new CompileRequest();
+        compileRequest.setSource(code);
+        compileRequest.setLang("java");
+
+        String apiKey = "API_KEY_AQUI";
+
+        return hackerEarthClient.compileCode(apiKey, compileRequest);
+    }
+}
