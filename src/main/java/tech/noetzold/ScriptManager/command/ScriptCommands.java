a8d9d362df312e5188e487ad97d91a047b153093
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-28 06:19:27 -0300

    Update ScriptCommands.java

--- src/main/java/tech/noetzold/ScriptManager/command/ScriptCommands.java
+++ src/main/java/tech/noetzold/ScriptManager/command/ScriptCommands.java
@@ -3,18 +3,25 @@ package tech.noetzold.ScriptManager.command;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import tech.noetzold.ScriptManager.service.CodeCompilationService;
 
 @ShellComponent
 public class ScriptCommands {
 
+    private final CodeCompilationService codeCompilationService;
+
+    public ScriptCommands(CodeCompilationService codeCompilationService) {
+        this.codeCompilationService = codeCompilationService;
+    }
+
     @ShellMethod(key = "show_all", value = "List the scripts")
     public void showAll(){
-
+        System.out.println();
     }
 
     @ShellMethod(key = "add_new", value="Add new script")
     public void addNew(@ShellOption String lang, @ShellOption String pathSource){
-
+        System.out.println(codeCompilationService.compileCode(lang, pathSource));
     }
 
     @ShellMethod(key = "compile", value = "Compile a script")
