4a59a7cd1cd123eb0e562089bf146edae4351adf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-29 08:07:27 -0300

    Update ScriptCommands.java

--- src/main/java/tech/noetzold/ScriptManager/command/ScriptCommands.java
+++ src/main/java/tech/noetzold/ScriptManager/command/ScriptCommands.java
@@ -21,12 +21,12 @@ public class ScriptCommands {
 
     @ShellMethod(key = "add_new", value="Add new script")
     public void addNew(@ShellOption String lang, @ShellOption String pathSource){
-        System.out.println(codeCompilationService.compileCode(lang, pathSource));
+        System.out.println(codeCompilationService.addNewScript(lang, pathSource).toString());
     }
 
     @ShellMethod(key = "compile", value = "Compile a script")
-    public void compileScript(){
-
+    public void compileScript(@ShellOption Long id){
+        System.out.println(codeCompilationService.compileCode(id));
     }
     @ShellMethod(key = "result", value = "See the compile result")
     public void seeResult(){
