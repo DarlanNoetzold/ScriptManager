514d4b10ff5aee03468f1d9ab6ce70d32c396cce
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-29 08:07:32 -0300

    Update CodeCompilationService.java

--- src/main/java/tech/noetzold/ScriptManager/service/CodeCompilationService.java
+++ src/main/java/tech/noetzold/ScriptManager/service/CodeCompilationService.java
@@ -4,23 +4,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.ScriptManager.client.HackerEarthClient;
 import tech.noetzold.ScriptManager.model.CompileRequest;
+import tech.noetzold.ScriptManager.repository.CompileRepository;
+
+import java.util.List;
 
 @Service
 public class CodeCompilationService {
     private final HackerEarthClient hackerEarthClient;
 
     @Autowired
+    private CompileRepository compileRepository;
+
+    @Autowired
     public CodeCompilationService(HackerEarthClient hackerEarthClient) {
         this.hackerEarthClient = hackerEarthClient;
     }
 
-    public String compileCode(String lang, String code) {
+    public CompileRequest addNewScript(String lang, String code){
         CompileRequest compileRequest = new CompileRequest();
         compileRequest.setSource(code);
         compileRequest.setLang(lang);
 
+        return compileRepository.save(compileRequest);
+    }
+
+    public String compileCode(Long id) {
+        CompileRequest compileRequest = compileRepository.findById(id).orElse(null);
+
+        if(compileRequest == null)
+            return "There are no script with id " + id;
+
         String apiKey = "cdd1b13d25456b4c72a2ef03c5b2e8643527f5a9";
 
         return hackerEarthClient.compileCode(apiKey, compileRequest);
+    }
+
+    public List<CompileRequest> showScripts(){
+        return compileRepository.findAll();
     }
 }
