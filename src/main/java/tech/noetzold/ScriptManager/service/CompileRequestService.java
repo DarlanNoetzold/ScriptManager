6e89cad3341f45c9534d9b923fa404d4cbd55314
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-30 06:26:50 -0300

    Update CompileRequestService.java

--- src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
+++ src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
@@ -1,8 +1,22 @@
 package tech.noetzold.ScriptManager.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.ScriptManager.model.CompileRequest;
+import tech.noetzold.ScriptManager.repository.CompileRepository;
 
 @Service
 public class CompileRequestService {
+
+    @Autowired
+    private CompileRepository compileRepository;
+
+    public CompileRequest addNewScript(String lang, String code){
+        CompileRequest compileRequest = new CompileRequest();
+        compileRequest.setSource(code);
+        compileRequest.setLang(lang);
+
+        return compileRepository.save(compileRequest);
+    }
 
 }
