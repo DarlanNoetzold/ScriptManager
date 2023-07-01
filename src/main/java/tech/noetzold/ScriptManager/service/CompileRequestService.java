30a9bd1165b3238a518e4b7a6e495af55e185811
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-07-01 07:51:44 -0300

    Update CompileRequestService.java

--- src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
+++ src/main/java/tech/noetzold/ScriptManager/service/CompileRequestService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import tech.noetzold.ScriptManager.model.CompileRequest;
 import tech.noetzold.ScriptManager.repository.CompileRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CompileRequestService {
 
@@ -17,6 +20,32 @@ public class CompileRequestService {
         compileRequest.setLang(lang);
 
         return compileRepository.save(compileRequest);
+    }
+
+    public List<CompileRequest> getAllScripts(){
+        return compileRepository.findAll();
+    }
+
+    public CompileRequest getScriptById(Long id){
+        return compileRepository.findById(id).orElse(null);
+    }
+
+    public void removeSriptById(Long id){
+        compileRepository.deleteById(id);
+    }
+
+    public CompileRequest updateScript(CompileRequest compileRequest){
+        Optional<CompileRequest> compileRequestExist = compileRepository.findById(compileRequest.getId());
+        if (compileRequestExist.isEmpty()) {
+            throw new RuntimeException("Entidade não encontrada");
+        }
+
+        CompileRequest compileRequestUpdate= compileRequestExist.get();
+        compileRequestUpdate.setLang(compileRequest.getLang());
+        compileRequestUpdate.setSource(compileRequest.getSource());
+
+
+        return compileRepository.save(compileRequestUpdate);
     }
 
 }
