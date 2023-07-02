be601aecd46a7eaa552b9a6ad342c4ab7e84c13c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-07-02 08:11:27 -0300

    Update HackerEarthClient.java

--- src/main/java/tech/noetzold/ScriptManager/client/HackerEarthClient.java
+++ src/main/java/tech/noetzold/ScriptManager/client/HackerEarthClient.java
@@ -1,10 +1,12 @@
 package tech.noetzold.ScriptManager.client;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import tech.noetzold.ScriptManager.model.CompileRequest;
 
+@Service
 @FeignClient(name = "hackerEarthClient", url = "https://api.hackerearth.com/v4/partner/code-evaluation/submissions/")
 public interface HackerEarthClient {
 
