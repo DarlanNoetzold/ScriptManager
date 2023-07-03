6b768fca1b3f34c226d721117830740a7afe6d1c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-07-03 09:31:58 -0300

    Update HackerEarthClient.java

--- src/main/java/tech/noetzold/ScriptManager/client/HackerEarthClient.java
+++ src/main/java/tech/noetzold/ScriptManager/client/HackerEarthClient.java
@@ -1,15 +1,16 @@
 package tech.noetzold.ScriptManager.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 import tech.noetzold.ScriptManager.model.CompileRequest;
 
 @Service
-@FeignClient(name = "hackerEarthClient", url = "https://api.hackerearth.com/v4/partner/code-evaluation/submissions/")
+@FeignClient(name = "hackerEarthClient", url = "https://api.hackerearth.com/v4/partner")
 public interface HackerEarthClient {
 
-    @PostMapping
+    @PostMapping("/code-evaluation/submissions")
     String compileCode(@RequestHeader("client-secret") String apiKey, @RequestBody CompileRequest compileRequest);
+
+    @GetMapping("/code-evaluation/submissions/{id}")
+    String resultCompileCode(@RequestHeader("client-secret") String apiKey, @PathVariable("id") String id);
 }
